86b620eea432a6d6d0517f8ebd8caa0d906a72e9
Yoel Hartman <yoel2810@gmail.com>
2023-12-23 15:42:47 +0200

    Delete Main.gd and Main.tscn, update main_scene in project.godot

--- Scenes/Grid.gd
+++ Scenes/Grid.gd
@@ -3,6 +3,7 @@ extends Node2D
 var grid_size: Vector2 = Vector2(10, 10)
 var cell_size: Vector2 = Vector2(32, 32)
 var grid: Array = []
+const GAP: int = 2
 
 
 func _ready() -> void:
@@ -19,17 +20,13 @@ func setup_grid() -> void:
 func create_grid_cell(x: int, y: int) -> Control:
 	var cell: Control = Control.new()
 	cell.size = cell_size
-	cell.position = Vector2(x * cell_size.x, y * cell_size.y)
-
-	# Customize the appearance of the cell (e.g., add a Sprite)
+	cell.position = Vector2(x * (cell_size.x + GAP), y * (cell_size.y + GAP)) + cell_size / 2
 	var sprite: Sprite2D = Sprite2D.new()
-	sprite.texture = load("res://icon.svg")
+	sprite.texture = load("res://Assets/white.png")
 	cell.add_child(sprite)
 	sprite.scale = Vector2(
 		cell_size.x / sprite.texture.get_width(), cell_size.y / sprite.texture.get_height()
 	)
-
-	# Add the cell as a child of the main node
 	add_child(cell)
 
 	return cell
